21a6581152fa3b07d99692b4adfcffa0e8076656
andriko41 <tosa_andras@hotmail.com>
2024-09-26 10:34:28 +0300

    3 lecke kisebb vizualis javitasok

--- gyakorlat.js
+++ gyakorlat.js
@@ -2,56 +2,59 @@
 // 1 feladat forditott tomb
 
 const tomb1 = [1,2,3,4,5,6];
-console.log('tomb1', tomb1)
+console.log('tomb1', tomb1);
 
-const forditott = tomb1.reverse ()
-console.log('forditott', forditott)
+const forditott = tomb1.reverse ();
+console.log('tomb1 forditott', forditott);
 
 // 2 feladat paros szamok
 
 let tomb2 = [10,21,30,39,42];
+console.log('tomb2',tomb2);
 let paros = []
 for (let i = 0; i < tomb2.length; i++)
 {
     if (tomb2[i] % 2 == 0)
    paros.push(tomb2[i]); 
 }
-console.log('paros', paros)
+console.log('tomb2 paros', paros);
 
 // 3 feladat tomb elem szorzasa 2-vel
 
 let tomb3 = [1,2,4,6,9];
+console.log('tomb3', tomb3);
 for (var i=0; i<tomb3.length; i++) {
     tomb3[i] *= 2;
 }
-console.log('tomb3',tomb3)
+console.log('tomb3 Szorzas',tomb3);
 
-// 4 feladat szam megvizsgalas a tomben
+// 4 feladat szam megvizsgalas a tombben
 
-
-console.log([6,11,22,36,41].includes(11))
+const tomb4 = [6,11,22,36,41];
+console.log('tomb4', tomb4);
+console.log('benne van a 11 =',tomb4.includes(11));
 
 //vagy
 
-const tomb4 = [6,11,22,36,41];
-
 const talalt = tomb4.find((element) => element > 10);
 
-console.log(talalt);
+console.log('vagy talalt', talalt);
 
 // 5 feladat novekvo sorrend
     
 const tomb5 = [23,4,126,7,32];
-tomb5.sort((a, b) => a - b)
-console.log(tomb5)
+console.log('tomb5', tomb5);
+tomb5.sort((a, b) => a - b);
+console.log('tomb5 novekvo', tomb5);
 
 // 6 feladat duplikalt
 
 let tomb6 = [2,4,4,7,9,12,22,9];
+console.log('tomb6', tomb6)
 
 function duplikalteltavolitas(tomb6) 
 {
 return tomb6.filter((item,
   index) => tomb6.indexOf(item) === index);
 }
-console.log(duplikalteltavolitas(tomb6));
+console.log('tomb6 duplikalt', duplikalteltavolitas(tomb6));
